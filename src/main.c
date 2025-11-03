157d3c1ecc972542456cba015e584c0b4555c0b6
aymen-bc <aymenbencheikh789@gmail.com>
2025-11-03 23:40:31 +0100

    resize image to standard output width

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image.h"
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
 
-unsigned char *resize_img();
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "../include/stb_image_resize2.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> 
+#include <sys/ioctl.h>
+
+
 // char* avg_brightness(unsigned char* pixel);
 void print_colored_char(char c, int r, int g, int b);
 
@@ -14,29 +20,53 @@ int main(int argc, char** argv){
     printf("Usage: %s <image_path>\n", argv[0]);
     return -1;
   }else{
+
+    // Get infos about standard output
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
     int width, height, channels;
     unsigned char *img = stbi_load(argv[argc-1], &width, &height, &channels, 3);
     if (!img) {
       printf("Failed to load image\n");
       return -1;
     }
+
+    // Get width and height of standard output
+    int newWidth = w.ws_col;
+    float scale = (float)newWidth / width; 
+    int newHeight = height * scale;
+    unsigned char *newImg = (unsigned char*) malloc (newHeight * newWidth * channels);
+
+    if (!newImg) {
+      printf("Failed to allocate image\n");
+      return -1;
+    }
+
+    // Resize image and put it in newImg variable
+    stbir_resize_uint8_linear(
+      img, width, height, width * channels, 
+      newImg, newWidth, newHeight, newWidth * channels, 
+      channels
+    );
+
     char *brightness = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
     int brightness_level = strlen(brightness);
-    for (int y = 0; y < height; y+=2){
-      for(int x = 0; x < width; x++){
+    for (int y = 0; y < newHeight; y+=2){
+      for(int x = 0; x < newWidth; x++){
 
-        int pixel_index1 = (x + y * width) * channels;
-        int pixel_index2 = (x + (y + 1) * width) * channels;
+        int pixel_index1 = (x + y * newWidth) * channels;
+        int pixel_index2 = (x + (y + 1) * newWidth) * channels;
 
         // Color of pixel of the current row
-        unsigned char red1 = img[pixel_index1];
-        unsigned char green1 = img[pixel_index1 + 1];
-        unsigned char blue1 = img[pixel_index1 + 2];
+        unsigned char red1 = newImg[pixel_index1];
+        unsigned char green1 = newImg[pixel_index1 + 1];
+        unsigned char blue1 = newImg[pixel_index1 + 2];
 
         // Color of pixel of the next row
-        unsigned char red2 = img[pixel_index2];
-        unsigned char green2 = img[pixel_index2 + 1];
-        unsigned char blue2 = img[pixel_index2 + 2];
+        unsigned char red2 = newImg[pixel_index2];
+        unsigned char green2 = newImg[pixel_index2 + 1];
+        unsigned char blue2 = newImg[pixel_index2 + 2];
 
         // if (y < 3) {
         //   printf("Pixel (%d,%d):", x, y);
@@ -57,15 +87,14 @@ int main(int argc, char** argv){
     }
     // printf("Loaded image: %dx%d, %d channels\n", width, height, channels);
     stbi_image_free(img);
+    free(newImg);
 
     return 0;
   }
 }
 
-unsigned char *resize_img(){
-  //Todo: checkout how resize a photo in c && stb_image_resize libraby
-}
-
 void print_colored_char(char c, int r, int g, int b){
   printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, c);
 }
+
+//Check out sobel operation && sobel edge detection
